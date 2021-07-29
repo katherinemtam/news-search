8ddbe14c300d023ffcdb6ebe14968b4f7c95c9b9
Katherine Tam <katherinemtam@gmail.com>
2021-07-28 22:26:54 -0700

    conditional rendering based on null values, remove isRequired

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -4,15 +4,20 @@ import PropTypes from 'prop-types';
 const Article = ({ title, author, description  }) => (
   <>
     <h2>{title}</h2>
-    <h3>{author}</h3>
-    <p>{description}</p>
+    {author 
+      ? (<h3>By {author}</h3>)
+      : (<h3>By Unknown Author</h3>)
+    }
+    {description
+      ? (<h3>{description}</h3>)
+      : (<h3>No description available.</h3>)}
   </>
 );
 
 Article.propTypes = {
   title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  author: PropTypes.string,
+  description: PropTypes.string
 };
 
 export default Article;
