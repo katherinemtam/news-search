fa16c4d02f34267c0aab099a08171f0338851dc9
Katherine Tam <katherinemtam@gmail.com>
2021-07-28 22:25:50 -0700

    getTopHeadlines function

--- src/services/news-utils.js
+++ src/services/news-utils.js
@@ -1,6 +1,15 @@
-export const getArticles = async (search) => {
+export const getSearchedArticles = async (search) => {
   // eslint-disable-next-line max-len
   const res = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${process.env.API_KEY}`);
+
+  const json = await res.json();
+
+  return json.articles;
+};
+
+export const getTopHeadlines = async () => {
+  // eslint-disable-next-line max-len
+  const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}`);
 
   const json = await res.json();
 
