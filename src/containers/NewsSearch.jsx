dc399c5d4bf69ca9e65eeced491350dfb245d3d0
Katherine Tam <katherinemtam@gmail.com>
2021-07-28 22:28:05 -0700

    add top headlines on intial load, add loading state

--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import ArticleList from '../components/articles/ArticleList';
 import Search from '../components/search/Search';
-import { getArticles } from '../services/news-utils';
+import { getSearchedArticles, getTopHeadlines } from '../services/news-utils';
 
 export default class NewsSearch extends Component {
   state = {
     search: '',
     articles: [],
-    // loading: true,
+    loading: true,
   };
 
-  // async componentDidMount() {
-  //   const articles = await getArticles(this.state.search);
-  //   this.setState({ articles });
-  // }
+  async componentDidMount() {
+    const articles = await getTopHeadlines();
+    this.setState({ articles, loading: false });
+  }
 
   handleChange = ({ target }) => {
     this.setState({ search: target.value });
@@ -21,13 +21,14 @@ export default class NewsSearch extends Component {
 
   handleSubmit = async (e) => {
     e.preventDefault();
-    const articles = await getArticles(this.state.search);
-    this.setState({ articles });
+    this.setState({ articles, loading: true });
+    const articles = await getSearchedArticles(this.state.search);
+    this.setState({ articles, loading: false });
   }
 
   render() {
     const { search, articles, loading } = this.state;
-    // if(loading) return <h1>Loading...</h1>;
+    if(loading) return <h1>Loading...</h1>;
 
     return (
       <>
