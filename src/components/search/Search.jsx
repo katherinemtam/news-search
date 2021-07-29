f450f6f58e29c8e47f648ff46c856562ab854574
Katherine Tam <katherinemtam@gmail.com>
2021-07-28 22:06:29 -0700

    Search bar component

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Search = ({ search, onChange, onSearch, onSubmit }) => (
+  <form onSubmit={onSubmit}>
+    <label htmlFor="search">Search:</label>
+    <input 
+      id="search"
+      type="search"
+      name="search"
+      value={search}
+      onChange={onChange}
+    />
+    <button>Submit</button>
+  </form>
+);
+
+Search.propTypes = {
+  search: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
+
+export default Search;
