377619583bc1f7efe5d2551a88a813df859e6aaf
Katherine Tam <katherinemtam@gmail.com>
2021-07-28 22:06:43 -0700

    ArticleList component

---
+++ src/components/articles/ArticleList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Article from './Article';
+
+const ArticleList = ({ articles }) => {
+
+  const articleElements = articles.map(article => (
+    <li key={article.title}>
+      <Article
+        title={article.title}
+        author={article.author}
+        description={article.description}
+      />
+    </li>
+  ));
+
+  return <ul>{articleElements}</ul>;
+};
+
+ArticleList.propTypes = {
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired
+    })
+  ).isRequired,
+};
+
+export default ArticleList;
+
