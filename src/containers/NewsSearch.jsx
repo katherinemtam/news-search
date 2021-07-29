8be50997a2f1a8ded66e4b2833f11c6e2991bfa6
Katherine Tam <katherinemtam@gmail.com>
2021-07-28 23:27:03 -0700

    add initialLoad state and article list descriptor

--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -8,6 +8,7 @@ export default class NewsSearch extends Component {
     search: '',
     articles: [],
     loading: true,
+    initialLoad: true,
   };
 
   async componentDidMount() {
@@ -23,23 +24,26 @@ export default class NewsSearch extends Component {
     e.preventDefault();
     this.setState({ articles, loading: true });
     const articles = await getSearchedArticles(this.state.search);
-    this.setState({ articles, loading: false });
+    this.setState({ articles, loading: false, initialLoad: false });
   }
 
   render() {
-    const { search, articles, loading } = this.state;
+    const { search, articles, loading, initialLoad } = this.state;
     if(loading) return <h1>Loading...</h1>;
 
     return (
       <>
+        <h1>Daily Tribune</h1>
         <Search 
           search={search}
           onChange={this.handleChange}
-          onSearch={this.handleSearch}
           onSubmit={this.handleSubmit}
         />
-
-        <ArticleList articles={articles}/>
+        {initialLoad
+          ? <h2>Top Headlines</h2>
+          : <h2>Here's your search results for '{search}':</h2>
+        }
+        <ArticleList search={search} articles={articles}/>
       </>
     );
   }
